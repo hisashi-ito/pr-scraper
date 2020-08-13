3c5be53b8645466d6f4445f9c5f0b2616cb793fd
Hisashi Itoh <it.hisas@gmail.com>
2020-08-13 13:55:26 +0900

    update

--- cgi/db/update.rb
+++ cgi/db/update.rb
@@ -27,13 +27,15 @@ class DB
         # db にlink のデータがあるか確認する 
         sql = "select link from pr_table where link = '#{link}'"
         ret = @db.execute(sql)
+        
         # 重複データがあるのでスキップ
         next if ret.size != 0
         # データの登録
         sql = "insert into pr_table (domain, unix_time, display_time, title, link, body) values ('#{domain}', '#{unix_time}', '#{display_time}', '#{title}', '#{link}', '#{body}')"
         @db.execute(sql)
-      rescue
-        @logger.error("DB操作時にエラーが発生しました")
+      rescue Exception => e
+        @logger.error("DB操作時にエラーが発生しました: #{e.backtrace.join("\n")}")
+        next
       end
     end
   end
